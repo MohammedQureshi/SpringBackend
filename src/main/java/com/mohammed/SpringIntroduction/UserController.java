4ffc3f4acfcb640438b33bb1bc742c724e98b361
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-03-19 18:33:17 +0000

    Added support for reading form on front end

--- src/main/java/com/mohammed/SpringIntroduction/UserController.java
+++ src/main/java/com/mohammed/SpringIntroduction/UserController.java
@@ -1,7 +1,10 @@
 package com.mohammed.SpringIntroduction;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,23 +21,39 @@ public class UserController {
         );
     }
 
+//    @GetMapping("/testUsers")
+//    @CrossOrigin(origins = "http://localhost:3000")
+//    public List<UserDetails> testUsers(){
+//        return Arrays.asList(
+//                new UserDetails(1, "Shauna", "Jada", 24, "34 Evergreen Road, Lowestoft,NR32 2SA", "Female"),
+//                new UserDetails(2, "Dexter ", "Schuyler", 21, "J J B Soccer Dome, Trafford Way, Dumplington,M17 8DD", "Male"),
+//                new UserDetails(3, "Deidre ", "Paisley", 22, "11 Meadow Lane, Leasingham,NG34 8LL", "Male"),
+//                new UserDetails(4, "Madoline ", "Charlotte", 29, "Hewletts Mill, Galhampton,BA22 7BG", "Male")
+//        );
+//    }
+
+    private List<UserDetails>  listStrings = new ArrayList<UserDetails>();
+
     @GetMapping("/testUsers")
     @CrossOrigin(origins = "http://localhost:3000")
     public List<UserDetails> testUsers(){
-        return Arrays.asList(
-           new UserDetails(1, "Shauna", "Jada", 24, "34 Evergreen Road, Lowestoft,NR32 2SA", "Female"),
-           new UserDetails(2, "Dexter ", "Schuyler", 21, "J J B Soccer Dome, Trafford Way, Dumplington,M17 8DD", "Male"),
-           new UserDetails(3, "Deidre ", "Paisley", 22, "11 Meadow Lane, Leasingham,NG34 8LL", "Male"),
-           new UserDetails(4, "Madoline ", "Charlotte", 29, "Hewletts Mill, Galhampton,BA22 7BG", "Male")
-        );
+        return listStrings;
     }
 
-
+//    @CrossOrigin(origins = "http://localhost:3000")
+//    @PostMapping("/reactdata")
+//    public String postFoos(@RequestBody String request) {
+//        System.out.println(request);
+//        return request;
+//    }
+    private int id = 0;
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/reactdata")
-    @ResponseBody
-    public String postFoos() {
-        System.out.println("THIS WORKED!");
-        return "I just got some data from the frontend";
+    public String postFoos(@RequestBody UserDetails request) {
+        id++;
+        UserDetails user = new UserDetails(id, request.getFirstName(), request.getLastName(), request.getAge(), request.getAddressLine(), request.getGender());
+        listStrings.add(user);
+        return "Success";
     }
+
 }
